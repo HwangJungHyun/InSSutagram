a3c6e6b06dba3f53f8790fa7072877af3cec152e
HwangJungHyun <f7817455@naver.com>
2021-12-12 06:34:24 +0900

    데이터 베이스 연결 및 로그인 초기 구현

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
-//const { sequelize } = require('./models');
+const authRouter = require('./routes/auth');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
-app.set('port', process.env.PORT || 8001);
+passportConfig(); // 패스포트 설정
+app.set('port', process.env.PORT || 8055);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
   watch: true,
 });
-/*
+
 sequelize.sync({ force: false })
   .then(() => {
     console.log('데이터베이스 연결 성공');
@@ -25,7 +29,7 @@ sequelize.sync({ force: false })
   .catch((err) => {
     console.error(err);
   });
-*/
+
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -40,8 +44,11 @@ app.use(session({
     secure: false,
   },
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
